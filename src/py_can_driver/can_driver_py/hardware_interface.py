a65884ac969e0e61300b1963fe7a66757eaa94a3
Yuchen Lin <y29lin@uwaterloo.ca>
2024-11-12 19:12:34 +0000

    add yaml file reader support

--- src/py_can_driver/can_driver_py/hardware_interface.py
+++ src/py_can_driver/can_driver_py/hardware_interface.py
@@ -1,13 +1,25 @@
-# Import Sys Packages
-import CAN
-import sys
-import glob
-import serial
+# Import p Packages
+import can
+import yaml 
 
 
 class CanInstance:
     def __init__(self):
         # Check platform, read yml whitelist config
         # Try expect and enable the interface
-        self.can_bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
-        msg = bus.recv(None)        
+        # self.can_bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
+        pass
+    
+    def read_config(self):
+        with open('config/interface.yaml', 'r') as file:
+            devices = yaml.safe_load(file)
+            print(devices)
+
+
+
+def main(args=None):
+    test = CanInstance()
+    test.read_config()
+
+if __name__ == '__main__':
+    main()
