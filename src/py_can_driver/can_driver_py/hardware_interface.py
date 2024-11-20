9d5f2290ca97f770885f028bd00a149cc204cffd
Yuchen Lin <y29lin@uwaterloo.ca>
2024-11-20 00:32:22 -0500

    impl singleton

--- src/py_can_driver/can_driver_py/hardware_interface.py
+++ src/py_can_driver/can_driver_py/hardware_interface.py
@@ -1,33 +1,57 @@
-# Import p Packages
+# Import python Packages
 import can
 import yaml 
 
 # TODO:
-# Impl singleton for all the can interface and each have an attribute
+# Proper documentation
+
+# This is Parent class(for all the new can device)
 class CanInstance:
-    def __init__(self):
-        # Check platform, read yml whitelist config
-        # Try expect and enable the interface
-        # self.can_bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
-        self.read_config()
-        self.get_device()
-    
+    _instance = None
+
+    def __new__(cls, arg1, arg2):
+        if cls._instance is None:
+            cls._instance = super(CanInstance, cls).__new__(cls)
+            try:
+                # Var
+                self.can_bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=500000)
+            # Find runtime error
+            except:
+
+
+
+
+# Contain a dictionary of can interface instance also singleton
+class FindDevice:
+    _instance = None
+    CONFIG = 'config/interface.yaml'
+    # @return: dict:(key, CanInstance object)
+    can_devices = []
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(FindDevice, cls).__new__(cls)
+            # init code here
+        # can interface code
+        cls._instance.read_config()
+        cls._instance.get_device()
+        return cls._instance
+
     def read_config(self):
-        with open('config/interface.yaml', 'r') as file:
+        with open(CONFIG, 'r') as file:
             self.devices = yaml.safe_load(file)
-        
+    
     def get_device(self):
         try:
             self.devices = self.devices["devices"]
         except KeyError:
             print("No valid CAN interface registered")
+        for device in self.devices:
+            print(device)
 
-
-
+        
 
 def main(args=None):
-    test = CanInstance()
-    test.read_config()
 
 if __name__ == '__main__':
     main()
