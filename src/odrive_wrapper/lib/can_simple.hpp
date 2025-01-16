6faf87d518c7aef16e01d5957c9b29cad1a6cff6
billylin609 <linyuchen830@gmail.com>
2025-01-16 08:51:45 -0500

    [statsh code]

--- src/odrive_wrapper/lib/can_simple.hpp
+++ src/odrive_wrapper/lib/can_simple.hpp
@@ -5,16 +5,35 @@
 #include <algorithm>
 #include <bit>
 
+#define arbitration_mask(uint8_t axis, uint8_t cmd) { return axis<<5|cmd; }
+#define CAN_MSG_MAX_SIZE 8
+
 class Msg {
 		/*complete this class*/
 		virtual encode(uint8_t &msg) = 0;
 		virtual decode(uint8_t &msg) = 0;
+		
+		virtual get_id() = 0;
+		virtual get_msg() = 0;
+
 		virtual ~Msg() = 0;
 }
 
 class CanMessage: Msg {
 		public
-				CanMessage(uint8_t id, uint8_t cmd, bool rtr, uint8_t dlc, uint8_t *buf);
+			CanMessage(uint8_t axis, uint8_t cmd, bool rtr, uint8_t dlc, uint8_t *buf)
+				: id(arbitration_mask(axis, cmd)), rtr(rtr) {
+					if (dlc <= CAN_MSG_MAX_SIZE) {
+						this -> dlc = dlc;
+						this -> msg = buf
+					} else {
+						throw std::range_error::range_error(std:format("CAN msg buffer out of bound. Expected MAX SIZE == {}. Received MAX SIZE == {}", CAN_MSG_MAX_SIZE, dlc));
+					}
+			}
+
+			
+
+				
 		private:
 				/* CAN Message format */
 				uint32_t id = 0x000;
