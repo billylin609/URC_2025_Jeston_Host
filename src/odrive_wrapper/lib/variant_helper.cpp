e8a19cf642baa6fd612785b6344b443d8e8a922b
billylin609 <linyuchen830@gmail.com>
2025-01-17 11:15:23 -0500

    f

--- src/odrive_wrapper/lib/variant_helper.cpp
+++ src/odrive_wrapper/lib/variant_helper.cpp
@@ -4,35 +4,85 @@
 //#include <variant>
 #include <cstdint>
 
-typedef struct {
-  bool val;
-  bool pos;
-} bool_t;
+using CustomDataType {
+  /**
+   * example: type_t(val, ..., sizeof(type_t)) 
+   */
+  
+
+  // check syntax duplicate variable name
+  // pass by reference?
+  static_assert(sizeof(bool) == 1)
+  class bool_t {
+    public:
+      bool_t(bool val, uint8_t pos, uint8_t size)
+        : val(val), pos(pos), buf(val<<pos) {
+          assert(size == this->size);
+      }
+
+      uint8_t getsize() { return this->size; }
+    private:
+      bool val;
+      bool pos;
+      uint8_t buf;
+      uint8_t size = 1;
+  };
+
+  class uint_t {
+    public:
+      uint_t(auto val, uint8_t size)
+        : val(dynamic_cast<uint64_t>val), size(size) {
+          assert (this->size <= 8);
+      }
+
+      uint8_t getsize() { return this->size; }
+    private:
+      uint64_t val;
+      uint8_t size;
+  };
+
+  class int_t {
+    public:
+      int_t(auto val, uint8_t size)
+        : val(dynamic_cast<int64_t>val), size(size) {
+          assert (this->size <= 8);
+      }
+
+      uint8_t getsize() { return this->size; }
+    private:
+      int64_t val;
+      uint8_t size;
+  };
+
+  static_assert(sizeof(float) == 4); 
+  class float_t {
+    public:
+      int_t(float val)
+        : val(val) {
+      }
+
+      uint8_t getsize() { return this->size; }
+    private:
+      float val;
+      uint8_t size = 4;
+  }
+}
 
 namespace PayloadVariant {
   // Todo:
   // 1. bad impl -> see similiar impl https://stackoverflow.com/questions/36717725/c-same-variable-act-as-int-and-float
   // Note: int sizing using a variable
   // 2. variant and index?
-  union MsgPayload {
-    bool_t _b;
-    uint8_t _us;
-    int16_t _is;
-    uint32_t _u;
-    int32_t _i;
-    uint64_t _ul;
-    float _f;
-  };
 
-  enum DataType {
-    bool_t = 0,
-    uint8_t = 1,
-    int16_t = 2,
-    uint32_t = 3,
-    int32_t = 4,
-    uint64_t = 5,
-    float_t = 6
-  };
+  class Payload{
+    public:
+      uint8_t sizeof
+    private:
+      int_t d;
+      uint_t u;
+      float f;
+      bool_t b;
+  }
 
   class MsgBuf {
       MsgBuf(input, type)
