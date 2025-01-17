82eef1bbc97469f4a4c41d4f7bff3ac036611648
yuchen lin <linyuchen830@gmail.com>
2025-01-17 00:58:49 -0500

    f

--- src/odrive_wrapper/lib/variant_helper.cpp
+++ src/odrive_wrapper/lib/variant_helper.cpp
@@ -1,22 +1,53 @@
 #ifndef VARIANT_HELPER
 #define VARIANT_HELPER
 
-#include <variant/variant.h>
-#include <stdint/stdint.h>
+//#include <variant>
+#include <cstdint>
 
-template<class... Ts>
-struct overloads: Ts... { using Ts::operator()...; }
+typedef struct {
+  bool val;
+  bool pos;
+} bool_t;
 
-static_assert(sizeof(uint8_t) == 1);
-static_assert(sizeof(float) == 4);
-static_assert(sizeof(bool) == 1)
-using can_payload_t = std::variant<int, float, bool>;
+namespace PayloadVariant {
+  union MsgPayload {
+    bool_t _b;
+    uint8_t _us;
+    int16_t _is;
+    uint32_t _u;
+    int32_t _i;
+    uint64_t _ul;
+    float _f;
+  };
 
-// Generic lambda visitor, handles both int and string types
-struct CallPrintName {
-    void operator()(const Derived& d) { d.PrintName(); }    
-    void operator()(const ExtraDerived& ed) { ed.PrintName(); }    
-};
+  enum DataType {
+    bool_t = 0,
+    uint8_t = 1,
+    int16_t = 2,
+    uint32_t = 3,
+    int32_t = 4,
+    uint64_t = 5,
+    float_t = 6
+  };
 
+  class MsgBuf {
+      MsgBuf(input, type)
+    private:
+      MsgPayload buf;
+      DataType type;
+  }
+
+  /** Todo: impl using variant
+   * static_assert(sizeof(uint8_t) == 1);
+   * static_assert(sizeof(float) == 4);
+   * static_assert(sizeof(bool) == 1)
+   * using can_payload_t = std::variant<int, float, bool_t>;
+
+   * // Generic lambda visitor, handles int, boolean, and float types
+   * // api: std::visit(visitor, val)
+   * auto vistor = [](const auto& val) -> auto { return mem_cast(val); }
+   **/
+
+}
 
 #endif //VARIANT_HELPER
